bd082cefbd32653d8b1b7a6241fa5d2eaf2253c3
liuq901 <liuq901@163.com>
2021-09-18 15:24:32 +0800

    finish examples

---
+++ blockcode/examples/circle.js
@@ -0,0 +1,7 @@
+file.examples.circle =
+[
+    ['Repeat', 10, [
+        ['Right', 36, 'degrees'],
+        ['Forward', 10, 'steps'],
+    ]],
+];
---
+++ blockcode/examples/triangle.js
@@ -0,0 +1,7 @@
+file.examples.triangle =
+[
+    ['Repeat', 3, [
+        ['Left', 120, 'degrees'],
+        ['Forward', 75, 'steps'],
+    ]],
+];
