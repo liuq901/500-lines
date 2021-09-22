1a912e7d143d1f246381b85207b0f81089f532aa
liuq901 <liuq901@163.com>
2021-09-22 11:35:22 +0800

    modify run_or_fail/update_repo from .py to .sh

--- ci/repo_observer.py
+++ ci/repo_observer.py
@@ -1,5 +1,10 @@
 import argparse
+import os
+import socket
 import subprocess
+import time
+
+import helpers
 
 def poll():
     parser = argparse.ArgumentParser()
@@ -13,9 +18,26 @@ def poll():
     dispatcher_host, dispatcher_port = args.dispatcher_server.split(':')
     while True:
         try:
-            subprocess.run(['python', 'update_repo.py', args.repo], check=True, capture_output=True, shell=True)
+            subprocess.run(['sh', 'update_repo.sh', args.repo], check=True, capture_output=True, shell=True)
         except subprocess.CalledProcessError as e:
             raise Exception(f'Could not update and check repository. Reason: {e.stderr.decode("gbk").strip()}')
+
+        if os.path.isfile('.commit_id'):
+            try:
+                response = helpers.communicate(dispatcher_host, int(dispatcher_port), 'status')
+            except socket.error as e:
+                raise Exception(f'Could not communicate with dispatcher server: {e}')
+            if response == 'OK':
+                commit = ''
+                with open('.commit_id', 'r') as f:
+                    commit = f.readline()
+                response = helpers.communicate(dispatcher_host, int(dispatcher_port), f'dispatch:{commit}')
+                if response != 'OK':
+                    raise Exception(f'Could not dispatch the test: {response}')
+                print('dispatched!')
+            else:
+                raise Exception(f'Could not dispatcher the test: {response}')
+        time.sleep(5)
 
 if __name__ == '__main__':
     poll()
